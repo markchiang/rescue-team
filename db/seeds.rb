d5a231a2973a811b51adb520eb2c6160b6ee871d
Yih-Shien Chiang <markchiang@gmail.com>
2018-10-16 11:49:09 +0800

    add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Uploader.create!([
+  {time: "2013-11-15 19:45:35", content: "2級", place: "花蓮縣"},
+  {time: "2013-11-15 19:45:35", content: "1級", place: "臺東縣"},
+  {time: "2013-11-15 19:45:35", content: "1級", place: "新北市"},
+  {time: "2013-11-15 19:45:35", content: "1級", place: "臺北市"},
+])
